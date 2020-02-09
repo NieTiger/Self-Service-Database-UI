93e93d63ddad4b23f1a2f02670567be581dbba00
achen01 <arnoldchen2020@u.northwestern.edu>
2020-02-09 13:12:01 -0600

    comments and fixed some warnings in history page

--- src/routes.js
+++ src/routes.js
@@ -18,18 +18,18 @@
 
 /* 2/4/2020 changed path, name, etc*/
 import Dashboard from "views/Dashboard.jsx";
-import FilterPage from "views/FilterPage.jsx";
-import PatientsPage from "views/PatientsPage.jsx";
-import PatientHistoryPage from "views/PatientHistoryPage.jsx";
-import PatientImagesPage from "views/PatientImagesPage.jsx";
+// import FilterPage from "views/FilterPage.jsx";
+// import PatientsPage from "views/PatientsPage.jsx";
+// import PatientHistoryPage from "views/PatientHistoryPage.jsx";
+// import PatientImagesPage from "views/PatientImagesPage.jsx";
 
-import UserProfile from "views/UserProfile.jsx";
-import TableList from "views/TableList.jsx";
-import Typography from "views/Typography.jsx";
-import Icons from "views/Icons.jsx";
-import Maps from "views/Maps.jsx";
-import Notifications from "views/Notifications.jsx";
-import Upgrade from "views/Upgrade.jsx";
+// import UserProfile from "views/UserProfile.jsx";
+// import TableList from "views/TableList.jsx";
+// import Typography from "views/Typography.jsx";
+// import Icons from "views/Icons.jsx";
+// import Maps from "views/Maps.jsx";
+// import Notifications from "views/Notifications.jsx";
+// import Upgrade from "views/Upgrade.jsx";
 
 const dashboardRoutes = [
   {
